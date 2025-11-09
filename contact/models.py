2b623313139f6482b483fda54c8a519b67e118ea
cyuber.devoloper <sabdumajid91@gmail.com>
2025-11-09 20:38:17 +0500

    coment

--- contact/models.py
+++ contact/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=100)
-    number = models.CharField(max_length=15)
-    message = models.TextField()
+    ism = models.CharField(max_length=50)
+    telefon = models.CharField(max_length=20)
+    email = models.EmailField()
+    tugilgan_sana = models.DateField()
+    qiziqishlar = models.TextField()
